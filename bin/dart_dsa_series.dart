fb8ca9bb803a4f2f0a3937b186393d3e25c87897
utterArt <ana.sub.46@gmail.com>
2024-12-03 13:52:31 +0530

    initial Commit: Integrating local to remote

---
+++ bin/dart_dsa_series.dart
@@ -0,0 +1,5 @@
+import 'package:dart_dsa_series/dart_dsa_series.dart' as dart_dsa_series;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_dsa_series.calculate()}!');
+}
---
+++ test/dart_dsa_series_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_dsa_series/dart_dsa_series.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
